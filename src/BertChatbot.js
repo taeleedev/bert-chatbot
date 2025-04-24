59ed1e6b883edcd9d8b9c63777014c7d2ed38161
taeleedev <leetk0720@gmail.com>
2025-04-24 15:29:45 +0900

    수정,,

--- src/BertChatbot.js
+++ src/BertChatbot.js
@@ -44,7 +44,7 @@ export default function BertChatbot() {
     }
 
     try {
-      const res = await fetch("https://ec2-3-35-21-64.ap-northeast-2.compute.amazonaws.com/chat", {
+      const res = await fetch(`${process.env.REACT_APP_API_BASE}/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: input }),
